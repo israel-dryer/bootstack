6dce34e11235481f7eeef83c469af588a6063605
Israel Dryer <israel.dryer@gmail.com>
2026-05-23 10:03:26 -0400

    feat(style): update badge assets and style

--- src/bootstack/style/builders/badge.py
+++ src/bootstack/style/builders/badge.py
@@ -2,21 +2,23 @@
 
 This module contains style builders for ttk.Label widget badge variants.
 """
+from typing import Optional
+
 from bootstack.style.bootstyle_builder_ttk import BootstyleBuilderTTk
 from bootstack.style.element import Element, ElementImage
 from bootstack.style.utility import recolor_element_image
 
 
 @BootstyleBuilderTTk.register_builder('square', 'TBadge')
-def build_default_badge_style(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, **options):
+def build_default_badge_style(b: BootstyleBuilderTTk, ttk_style: str, accent: Optional[str] = None, **options):
     build_badge(b, ttk_style, accent, 'square', **options)
 
 @BootstyleBuilderTTk.register_builder('pill', 'TBadge')
-def build_pill_badge_style(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, **options):
+def build_pill_badge_style(b: BootstyleBuilderTTk, ttk_style: str, accent: Optional[str] = None, **options):
     build_badge(b, ttk_style, accent, 'pill', **options)
 
 
-def build_badge(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, variant: str = 'square', **options):
+def build_badge(b: BootstyleBuilderTTk, ttk_style: str, accent: Optional[str] = None, variant: str = 'square', **options):
     """Create a badge style for the variant specified"""
 
     surface_token = options.get('surface', 'content')
@@ -24,7 +26,7 @@ def build_badge(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, vari
     normal = b.color(accent or 'primary')
     foreground = b.on_color(normal)
 
-    badge_img = recolor_element_image(f'badge_{variant}', normal, surface, surface, surface)
+    badge_img = recolor_element_image(f'badge_{variant}', normal, normal, surface, surface)
 
     b.create_style_element_image(ElementImage(f'{ttk_style}.border', badge_img.image, 
                                               border=badge_img.meta.border, 
